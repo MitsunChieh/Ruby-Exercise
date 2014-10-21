4b2b0a0caf6a4e1d89afdeedf2d872c52c755db4
MitsunChieh <Mitsun.Chieh@gmail.com>
2014-10-21 12:24:59 +0800

    basic 3

---
+++ basic/3.rb
@@ -0,0 +1,12 @@
+movie = {
+  movieA: 1975,
+  movieB: 2004,
+  movieC: 2013,
+  movieD: 2001,
+  movieE: 1981
+}
+puts movie[:movieA]
+puts movie[:movieB]
+puts movie[:movieC]
+puts movie[:movieD]
+puts movie[:movieE]
